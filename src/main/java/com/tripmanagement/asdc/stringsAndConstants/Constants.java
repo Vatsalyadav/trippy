42249437f48f46cb1b4cd3031e1a3ab71efa79f5
Sanika Tamhankar <sn295037@dal.ca>
2022-03-10 13:47:44 -0400

    VehicleOwner Customer Authentication

---
+++ src/main/java/com/tripmanagement/asdc/stringsAndConstants/Constants.java
@@ -0,0 +1,10 @@
+package com.tripmanagement.stringsAndConstants;
+
+public interface Constants {
+    public static String uType_Customer="CUSTOMER";
+    public static String uType_VehicleOwner="VEHICLE_OWNER";
+
+    
+
+    
+}
