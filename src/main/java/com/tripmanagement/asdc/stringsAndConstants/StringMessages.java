26d640fc75f53f49a93812aab1cf8e4e5aed12dd
Sanika Tamhankar <sn295037@dal.ca>
2022-03-10 14:06:34 -0400

    Constants and String messages

---
+++ src/main/java/com/tripmanagement/asdc/stringsAndConstants/StringMessages.java
@@ -0,0 +1,10 @@
+package com.tripmanagement.stringsAndConstants;
+
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
+
+public interface StringMessages {
+    
+    public static String NO_USER_FOUND="User does not exist";
+    public static String INCORRECT_AUTH="Incorrect email or password";
+    public static String SUCCESS="Success";
+}
