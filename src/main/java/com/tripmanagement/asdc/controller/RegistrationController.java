8c2223b0ced902748a93e1e103307a3c7062619c
Vatsal Yadav <vt769414@dal.ca>
2022-03-10 13:56:57 -0400

    Add default APIs for forgot password, register user and user dashboard pages

--- src/main/java/com/tripmanagement/asdc/controller/RegistrationController.java
+++ src/main/java/com/tripmanagement/asdc/controller/RegistrationController.java
@@ -1,13 +1,18 @@
 package com.tripmanagement.asdc.controller;
 
 import com.tripmanagement.asdc.model.UserLogin;
+import com.tripmanagement.asdc.service.RegistrationService;
 import com.tripmanagement.stringsAndConstants.StringMessages;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegistrationController {
+
+    @Autowired
+    RegistrationService registrationService;
 
     @RequestMapping("/")
     public String basePage() {
@@ -16,16 +21,38 @@ public class RegistrationController {
 
     @RequestMapping("/login")
     public String userLogin(UserLogin userLogin, Model model) {
-        // TODO : Add registrationService
-        // registrationService.loginUserWithEmailAndPassword(model.email, model.password);
-        // registrationService.isEmailValid()
-        String message = "getMessageFrom";
-            if(message.equalsIgnoreCase(StringMessages.SUCCESS))
-                return "dashboard";
-            else {
-                model.addAttribute("error_message", message);
-                return "login";
-            }
+        String message = registrationService.checkEmailPassword(userLogin.getEmail(), userLogin.getPassword());
+        if (message.equalsIgnoreCase(StringMessages.SUCCESS))
+            return "dashboard";
+        else {
+            model.addAttribute("error_message", message);
+            return "login";
+        }
+    }
+
+    /*
+     * Will return Forget Password Page
+     * */
+    @RequestMapping("/forgot-password")
+    public String forgotPassword() {
+        return "forgot-password";
+    }
+
+    /*
+     * Will return Forget Password Page
+     * */
+    @RequestMapping("/register-user")
+    public String registerUser() {
+        return "register";
+    }
+
+    /*
+     * Will open dashboard depending upon user type
+     * */
+    @RequestMapping("/dashboard")
+    public String dashboard() {
+        // TODO: open dashboard depending upon user type
+        return "dashboard";
     }
 
 }
