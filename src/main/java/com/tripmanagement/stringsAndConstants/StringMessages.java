6415edbede44f2a6c9ac13ad43cb3195dd826bec
Vatsal Yadav <vt769414@dal.ca>
2022-03-10 14:21:47 -0400

    String Constants for different states

---
+++ src/main/java/com/tripmanagement/stringsAndConstants/StringMessages.java
@@ -0,0 +1,8 @@
+package com.tripmanagement.stringsAndConstants;
+
+public interface StringMessages {
+
+    public static String NO_USER_FOUND = "User does not exist";
+    public static String INCORRECT_AUTH = "Incorrect email or password";
+    public static String SUCCESS = "Success";
+}
