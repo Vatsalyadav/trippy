a80de73ee0346ee68d030371b5c82e991b4d5653
Sanika Tamhankar <sn295037@dal.ca>
2022-03-15 20:38:41 -0300

    Vehicle Service and Fuel economy implementation

---
+++ src/main/java/com/tripmanagement/asdc/service/VehicleService.java
@@ -0,0 +1,15 @@
+package com.tripmanagement.asdc.service;
+
+import java.util.List;
+
+import com.tripmanagement.asdc.model.Vehicle;
+
+public interface VehicleService {
+
+	public void saveVehicle(Vehicle vehicle);
+	public Vehicle getVehicleDetails(int vehicle_id);
+	public List<Vehicle> getVehicles(int vehicleOwnerId);
+	public void updateFuelEconomy(int vehicle_id, float fuelEconomy);
+	public void deleteVehicle(int vehicleId);
+	
+}
