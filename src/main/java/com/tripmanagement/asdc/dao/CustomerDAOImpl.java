3108cefc513cdd89172c5ccf0719b649c3b942f1
Vatsal Yadav <vt769414@dal.ca>
2022-03-15 21:02:07 -0300

    Revert "Merge branch 'feature_backend' into 'test'"
    This reverts merge request !24

--- src/main/java/com/tripmanagement/asdc/dao/CustomerDAOImpl.java
+++ src/main/java/com/tripmanagement/asdc/dao/CustomerDAOImpl.java
@@ -1,9 +1,6 @@
- package com.tripmanagement.asdc.dao;
+package com.tripmanagement.asdc.dao;
 
 import com.tripmanagement.asdc.model.Customer;
-import com.tripmanagement.asdc.service.NotificationService;
-import com.tripmanagement.asdc.stringsAndConstants.StringMessages;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -15,9 +12,7 @@ public class CustomerDAOImpl implements CustomerDAO {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
-    
-    @Autowired
-    NotificationService notificationService;
+
 
 
     @Override
@@ -30,7 +25,7 @@ public class CustomerDAOImpl implements CustomerDAO {
     public void saveCustomer(Customer customer) {
         String sql = "insert into customer values("+customer.getCustomer_id()+","+customer.getCustomer_fname()+","+customer.getCustomer_lname()+","+customer.getMobile_no()+","+customer.getAddress()+","+customer.getEmail()+");";
         jdbcTemplate.update(sql);
-        notificationService.sendEmail(customer.getCustomer_fname()+ StringMessages.USER_REGISTERED_SUCCESSFULLY,StringMessages.AUTH_SUCCESSFUL,customer.getEmail());
+        //notificationService.sendEmail(customer.getCustomer_fname()+ StringMessages.USER_REGISTERED_SUCCESSFULLY,StringMessages.AUTH_SUCCESSFUL,customer.getEmail());
 
 
     }
