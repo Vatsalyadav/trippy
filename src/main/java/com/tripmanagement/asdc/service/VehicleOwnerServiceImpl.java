ec97e3f942f2ce7c11509f7065d75e4ed01f83fb
Vatsal Yadav <vt769414@dal.ca>
2022-03-17 05:50:00 -0300

    Extract Vehicle Owner details from User for saveVehicleOwner

--- src/main/java/com/tripmanagement/asdc/service/VehicleOwnerServiceImpl.java
+++ src/main/java/com/tripmanagement/asdc/service/VehicleOwnerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tripmanagement.asdc.service;
 
 import com.tripmanagement.asdc.dao.VehicleOwnerDAO;
+import com.tripmanagement.asdc.model.User;
 import com.tripmanagement.asdc.model.VehicleOwner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,9 +16,13 @@ public class VehicleOwnerServiceImpl implements VehicleOwnerService {
 	
 	@Override
 	@Transactional
-	public void saveVehicleOwner(VehicleOwner carOwner) {
-
-		vehicleOwnerDAO.saveVehicleOwner(carOwner);
+	public void saveVehicleOwner(User user) {
+		VehicleOwner vehicleOwner = new VehicleOwner();
+		vehicleOwner.setVehicleowner_fname(user.getFirst_name());
+		vehicleOwner.setVehicleowner_lname(user.getLast_name());
+		vehicleOwner.setEmail(user.getEmail());
+		vehicleOwner.setPassword(user.getPassword());
+		vehicleOwnerDAO.saveVehicleOwner(vehicleOwner);
 	}
 
 	@Override
