8abc338367dfa1d7e1c7c5abf2ecc603b535695a
Benny Tharigopala <bn489600@dal.ca>
2022-03-19 17:03:32 -0300

    Add DAO for Vehicles

--- src/main/java/com/tripmanagement/asdc/dao/VehicleDAO.java
+++ src/main/java/com/tripmanagement/asdc/dao/VehicleDAO.java
@@ -6,10 +6,10 @@ import com.tripmanagement.asdc.model.Vehicle;
 
 public interface VehicleDAO {
 
-	public Boolean addVehicle(Vehicle vehicle);
+	public boolean addVehicle(Vehicle vehicle);
 	public Vehicle getVehicleDetails(int vehicle_id);
 	public List<Vehicle> getVehicles(int vehicleOwnerId);
 	public void updateFuelEconomy(int vehicle_id, float fuelEconomy);
-	public Boolean deleteVehicle(int vehicleId);
+	public boolean deleteVehicle(int vehicleId);
 	
 }
