e3d9ef0a1789ce81c47b9049779e06baf0e197f4
Benny Tharigopala <bn489600@dal.ca>
2022-03-19 17:02:43 -0300

    Implement Service Methods for Trips

--- src/main/java/com/tripmanagement/asdc/service/TripServiceImpl.java
+++ src/main/java/com/tripmanagement/asdc/service/TripServiceImpl.java
@@ -1,9 +1,16 @@
 package com.tripmanagement.asdc.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.tripmanagement.asdc.dao.TripDAO;
+import com.tripmanagement.asdc.dao.VehicleDAO;
+import com.tripmanagement.asdc.dao.VehicleOwnerDAO;
+import com.tripmanagement.asdc.model.Ride;
 import com.tripmanagement.asdc.model.Trip;
+import com.tripmanagement.asdc.model.Vehicle;
+import com.tripmanagement.asdc.model.VehicleOwner;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,6 +20,12 @@ public class TripServiceImpl implements TripService {
 
 	@Autowired
 	TripDAO tripDAO;
+
+	@Autowired
+	VehicleDAO vehicleDAO;
+
+	@Autowired
+	VehicleOwnerDAO vehicleOwnerDAO;
 	
 	@Override
 	@Transactional
@@ -40,6 +53,27 @@ public class TripServiceImpl implements TripService {
 	public void deleteTrip(int trip_id) {
 		tripDAO.deleteTrip(trip_id);
 		
+	}
+
+	@Override
+	public List<Ride> getTripsList(String source, String destination) {
+		List<Ride> rideList=new ArrayList<>();
+		List<Trip> tripList=tripDAO.getTripsList(source, destination);
+		for(Trip trip:tripList)
+		{
+			Vehicle vehicle=vehicleDAO.getVehicleDetails(trip.getVehicle_id());
+			VehicleOwner vehicleOwner=vehicleOwnerDAO.getVehicleOwnerById(trip.getVehicleowner_id());
+			Ride ride=new Ride(trip.getTrip_id(), vehicle.getVehicle_name(), vehicle.getFuel_consumed(), vehicleOwner.getVehicleowner_fname(), vehicleOwner.getPhone(), calculateCost(vehicle,trip));
+			rideList.add(ride);
+		}
+		return rideList;
+	}
+
+	@Override
+	public float calculateCost(Vehicle vehicle, Trip trip) {
+
+		float cost=(float)(trip.getEstimated_kms()*vehicle.getFuel_economy()*(float)1.20)/vehicle.getAvailable_seats();
+		return cost;
 	}
 
 }
