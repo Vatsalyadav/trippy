a2c56329543993893cb774ddfc30dd24b1661343
Benny Tharigopala <bn489600@dal.ca>
2022-03-19 17:00:57 -0300

    Add Service for Trips

--- src/main/java/com/tripmanagement/asdc/service/TripService.java
+++ src/main/java/com/tripmanagement/asdc/service/TripService.java
@@ -2,7 +2,9 @@ package com.tripmanagement.asdc.service;
 
 import java.util.List;
 
+import com.tripmanagement.asdc.model.Ride;
 import com.tripmanagement.asdc.model.Trip;
+import com.tripmanagement.asdc.model.Vehicle;
 
 public interface TripService {
 
@@ -10,5 +12,7 @@ public interface TripService {
 	public Trip getTripDetails(int trip_id);
 	public List<Trip> getTripsList(int vehicleOwnerId);
 	public void deleteTrip(int trip_id);
+	public List<Ride> getTripsList(String source, String destination);
+	public float calculateCost(Vehicle vehicle,Trip trip);
 	
 }
