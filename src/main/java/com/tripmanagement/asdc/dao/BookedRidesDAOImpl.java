e53b4e962589173c2bb484d81cf14dc86d7b396b
Dharmay Dineshchandra Sureja <dh276903@dal.ca>
2022-03-27 14:06:56 -0300

    Add getpreviousrides to BookedRidesDAOImpl

--- src/main/java/com/tripmanagement/asdc/dao/BookedRidesDAOImpl.java
+++ src/main/java/com/tripmanagement/asdc/dao/BookedRidesDAOImpl.java
@@ -1,11 +1,11 @@
 package com.tripmanagement.asdc.dao;
 
 import com.tripmanagement.asdc.model.Booked_Rides;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,8 +14,8 @@ import java.util.List;
 public class BookedRidesDAOImpl implements BookedRidesDAO {
 
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	Logger logger = LoggerFactory.getLogger(BookedRidesDAOImpl.class);
 
@@ -23,9 +23,9 @@ public class BookedRidesDAOImpl implements BookedRidesDAO {
 	@Override
 	public boolean saveRide(Booked_Rides booked_rides) {
 		try{
-		String sql = "insert into booked_rides values("+null+",'"+booked_rides.getSource()+"','"+booked_rides.getDestination()+"',"+booked_rides.getDistance()+booked_rides.getVehicle_id()+","+booked_rides.getVehicleowner_id()+","+booked_rides.getCustomer_id()+","+booked_rides.getTimestamp()+","+booked_rides.getCost()+","+booked_rides.getFuel_economy()+");";
-        jdbcTemplate.update(sql);
-		return true;
+			String sql = "insert into booked_rides values("+null+",'"+booked_rides.getSource()+"','"+booked_rides.getDestination()+"',"+booked_rides.getDistance()+booked_rides.getVehicle_id()+","+booked_rides.getVehicleowner_id()+","+booked_rides.getCustomer_id()+","+booked_rides.getTimestamp()+","+booked_rides.getCost()+","+booked_rides.getFuel_economy()+");";
+			jdbcTemplate.update(sql);
+			return true;
 		}
 		catch(Exception e)
 		{
@@ -51,8 +51,22 @@ public class BookedRidesDAOImpl implements BookedRidesDAO {
 		}
 	}
 
+	@Override
+	public List<Booked_Rides> getPreviousRidesForCustomer(int customer_id, String timestamp) {
+		List<Booked_Rides> rides=new ArrayList<>();
+		try{
+			rides= jdbcTemplate.queryForList("select * from booked_rides where customer_id="+customer_id,
+					Booked_Rides.class);
+			return rides;
+		}
+		catch(Exception e)
+		{
+			logger.error("Error while getting previous ride for customer", e);
+			return rides;
+		}
+	}
 
-	
+
 }
 
 
