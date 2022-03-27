1a3792da5ba21c8458d395b6f1c94a24799301e9
Prakrut <pr298346@dal.ca>
2022-03-27 14:01:43 -0300

    Add exceptions and logger to VehicleOwnerDAOImpl

--- src/main/java/com/tripmanagement/asdc/dao/VehicleOwnerDAOImpl.java
+++ src/main/java/com/tripmanagement/asdc/dao/VehicleOwnerDAOImpl.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Repository
 public class VehicleOwnerDAOImpl implements VehicleOwnerDAO {
@@ -13,26 +15,55 @@ public class VehicleOwnerDAOImpl implements VehicleOwnerDAO {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-	@Override
-	public void saveVehicleOwner(VehicleOwner vehicleOwner) {
+	Logger logger = LoggerFactory.getLogger(VehicleOwnerDAOImpl.class);
 
+
+	@Override
+	public boolean saveVehicleOwner(VehicleOwner vehicleOwner) {
+		try{
         String sql = "insert into vehicleowner values("+null+",'"+vehicleOwner.getVehicleowner_fname()+"','"+vehicleOwner.getVehicleowner_lname()+"','"+vehicleOwner.getPhone()+"','"+vehicleOwner.getAddress()+"','"+vehicleOwner.getEmail()+"',"+vehicleOwner.getVehicleOwner_id()+",'"+vehicleOwner.getPassword()+"');";
         jdbcTemplate.update(sql);
 //		notificationService.sendEmail(vehicleOwner.getVehicleowner_fname()+StringMessages.USER_REGISTERED_SUCCESSFULLY,StringMessages.AUTH_SUCCESSFUL,vehicleOwner.getEmail());
+			return true;
+	}
+		catch(Exception e)
+		{
+			logger.error("Error saving vehicleOwner",e);
+			return false;
+
+		}
 		
 	}
 
 	@Override
 	public VehicleOwner getVehicleOwner(String email) {
+		if(email==null)
+		return null;
+		try{
 	   VehicleOwner carOwner=jdbcTemplate.queryForObject("select * from vehicleowner where email='"+email+"'",
        BeanPropertyRowMapper.newInstance(VehicleOwner.class));
 	  return carOwner;
+		}
+		catch(Exception e)
+		{
+			logger.error("Error getting vehicleOwner by email ",e);
+			return null;
+
+		}
 	}
 
 	@Override
 	public VehicleOwner getVehicleOwnerById(int vehicleOwnerId) {
+		try{
 		return jdbcTemplate.queryForObject("select * from vehicleowner where vehicleOwner_id="+vehicleOwnerId,
 				BeanPropertyRowMapper.newInstance(VehicleOwner.class));
+		}
+		catch(Exception e)
+		{
+			logger.error("Error getting vehicleOwner by id ",e);
+			return null;
+
+		}
 	}
 
 
