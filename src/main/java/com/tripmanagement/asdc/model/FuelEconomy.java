9f0909e5feb05fed71b663043dbe894c30fc5a9a
Vatsal Yadav <vt769414@dal.ca>
2022-03-27 14:29:32 -0300

    Add FuelEconomy model class

--- src/main/java/com/tripmanagement/asdc/model/FuelEconomy.java
+++ src/main/java/com/tripmanagement/asdc/model/FuelEconomy.java
@@ -1,7 +1,11 @@
 package com.tripmanagement.asdc.model;
 
-import javax.persistence.*;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 @Entity
 @Table(name="fuel_economy")
 public class FuelEconomy {
