cb42b82043005aa66733099dce1dcca353b5e0e9
Dharmay Dineshchandra Sureja <dh276903@dal.ca>
2022-04-02 15:12:53 -0300

    Add CustomerDAO test cases

---
+++ src/test/java/com/tripmanagement/asdc/dao/CustomerDAOImplTest.java
@@ -0,0 +1,76 @@
+package com.tripmanagement.asdc.dao;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.tripmanagement.asdc.model.Customer;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.sql.Date;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+class CustomerDAOImplTest {
+
+@Autowired
+CustomerDAO customerDAO;
+
+    @Test
+    void testSaveNullCustomer() {
+        assertFalse(customerDAO.saveCustomer(null));
+    }
+
+    @Test
+    void testSaveEmptyCustomer() {
+
+        assertFalse(customerDAO.saveCustomer(new Customer()));
+    }
+
+    @Test
+    void testSaveCustomer() {
+        if(customerDAO.getCustomerByEmail("abc@gmail.com")==null)
+
+        assertTrue(customerDAO.saveCustomer(new Customer(10,"test","case","885858585","test case street","test@case.com","1","123456667",22)));
+    }
+
+    @Test
+    void testGetCustomerByCorrectEmail() {
+    assertTrue(customerDAO.getCustomerByEmail("test@case.com")!=null);
+    }
+
+    @Test
+    void testGetCustomerByWrongEmail() {
+    assertNull(customerDAO.getCustomerByEmail("fjvnfk.vfjnvjf"));
+    }
+
+    @Test
+    void testGetCustomerBybadEmail() {
+    assertNull(customerDAO.getCustomerByEmail("dsd@dskal.com\'"));
+    }
+
+    @Test
+    void testGetCustomerByEmptyEmail() {
+    assertNull(customerDAO.getCustomerByEmail(""));
+    }
+
+    @Test
+    void testGetCustomerByWrongId(){
+        assertNull(customerDAO.getCustomerById(-1));
+    }
+    @Test
+    void testGetCustomerByCorrectId(){
+        assertNotNull(customerDAO.getCustomerById(1));
+
+    }
+    @Test
+    void testupdateAvaialableCredits(){
+        assertTrue(customerDAO.updateAvaialableCredits(9,22));
+    }
+}
