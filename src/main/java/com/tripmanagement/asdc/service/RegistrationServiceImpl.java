c658ae981ad8e90dd708aa6c406ffb74c3748f1a
Prakrut <pr298346@dal.ca>
2022-04-02 18:38:49 -0300

    Refactor ServiceStringMessage to resolve smells

--- src/main/java/com/tripmanagement/asdc/service/RegistrationServiceImpl.java
+++ src/main/java/com/tripmanagement/asdc/service/RegistrationServiceImpl.java
@@ -3,7 +3,7 @@ package com.tripmanagement.asdc.service;
 import javax.transaction.Transactional;
 
 import com.tripmanagement.asdc.dao.RegistrationDAO;
-import com.tripmanagement.asdc.stringsAndConstants.StringMessages;
+import com.tripmanagement.asdc.stringsAndConstants.ServiceStringMessages;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +21,12 @@ public class RegistrationServiceImpl implements RegistrationService{
     public String checkEmailPassword(String email, String password) {
         try{
             if (password == null || email == null) {
-                return StringMessages.INCORRECT_AUTH;
+                return ServiceStringMessages.INCORRECT_AUTH;
             } else if (password.isEmpty() || email.isEmpty()) {
-                return StringMessages.INCORRECT_AUTH;
+                return ServiceStringMessages.INCORRECT_AUTH;
             } else {
                 if (!registrationDAO.checkUserExistByEmail(email))
-                    return StringMessages.NO_USER_FOUND;
+                    return ServiceStringMessages.NO_USER_FOUND;
                 else
                     return registrationDAO.checkEmailPassword(email, password);
             }
