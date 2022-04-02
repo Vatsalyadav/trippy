1bb0a1c3f0e3c80a949a1de124f37e4947e5122e
Dharmay Dineshchandra Sureja <dh276903@dal.ca>
2022-04-02 17:36:41 -0300

    modify test case

--- src/test/java/com/tripmanagement/asdc/service/CustomerServiceImplTest.java
+++ src/test/java/com/tripmanagement/asdc/service/CustomerServiceImplTest.java
@@ -1,20 +1,28 @@
 package com.tripmanagement.asdc.service;
 
+import com.tripmanagement.asdc.dao.CustomerDAO;
 import com.tripmanagement.asdc.model.Customer;
 import com.tripmanagement.asdc.model.User;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 class CustomerServiceImplTest {
 
     @Autowired
     CustomerService customerService;
+
+    @Mock
+    CustomerDAO customerDAO;
+
 
     @Test
     void testSaveNullCustomer() {
@@ -31,12 +39,19 @@ class CustomerServiceImplTest {
 
         User user = new User();
         user.setFirst_name("test");
+        Customer customer = new Customer();
+        customer.setCustomer_fname("Sania");
+        customer.setCustomer_id(1);
+        customer.setCustomer_lname("kumar");
+        customer.setEmail("someemail@dal.ca");
+        customer.setPassword("password");
         user.setLast_name("case");
         user.setEmail("test@case.com");
         user.setPassword("123456");
         user.setUserType("CUSTOMER");
-
-            assertTrue(customerService.saveCustomer(user));
+        when(customerDAO.saveCustomer(customer)).thenReturn(true);
+        boolean result = customerService.saveCustomer(user);
+        assertTrue(result);
     }
     @Test
     void testSaveCustomerIllegaleName() {
@@ -47,44 +62,101 @@ class CustomerServiceImplTest {
         user.setEmail("test@case.com");
         user.setPassword("123456");
         user.setUserType("CUSTOMER");
-            assertFalse(customerService.saveCustomer(user));
+        Customer customer = new Customer();
+        customer.setCustomer_fname("Sania");
+        customer.setCustomer_id(1);
+        customer.setCustomer_lname("kumar");
+        customer.setEmail("someemail@dal.ca");
+        customer.setPassword("password");
+        when(customerDAO.saveCustomer(customer)).thenReturn(false);
+        boolean result = customerService.saveCustomer(user);
+        assertFalse(result);
     }
 
     @Test
     void testGetCustomerByCorrectEmail() {
-        assertTrue(customerService.getCustomerByEmail("svt@gmail.com")!=null);
+        Customer customer = new Customer();
+        customer.setCustomer_fname("Sania");
+        customer.setCustomer_id(1);
+        customer.setCustomer_lname("kumar");
+        customer.setEmail("someemail@dal.ca");
+        customer.setPassword("password");
+        String email= "svt@gmail.com";
+        when(customerDAO.getCustomerByEmail(email)).thenReturn(customer);
+        Customer result = customerService.getCustomerByEmail(email);
+        assertTrue(result!=null);
     }
-  @Test
+    @Test
     void testGetCustomerByIllegalEmail() {
-        assertNull(customerService.getCustomerByEmail("svt@gmail.com\'"));
+        String email= "svt@gmail.com\'";
+        when(customerDAO.getCustomerByEmail(email)).thenReturn(null);
+        Customer result = customerService.getCustomerByEmail(email);
+        assertNull(result);
     }
 
     @Test
     void testGetCustomerByWrongEmail() {
-        assertNull(customerService.getCustomerByEmail("fjvnfk.vfjnvjf"));
+        String email= "fjvnfk.vfjnvjf";
+        when(customerDAO.getCustomerByEmail(email)).thenReturn(null);
+        Customer result = customerService.getCustomerByEmail(email);
+        assertNull(result);
     }
 
     @Test
     void testGetCustomerByNullEmail() {
-        assertNull(customerService.getCustomerByEmail(null));
+        String email= null;
+        when(customerDAO.getCustomerByEmail(email)).thenReturn(null);
+        Customer result = customerService.getCustomerByEmail(email);
+        assertNull(result);
+       // assertNull(customerService.getCustomerByEmail(null));
     }
 
     @Test
     void testGetCustomerByEmptyEmail() {
-        assertNull(customerService.getCustomerByEmail(""));
+        String email= "";
+        when(customerDAO.getCustomerByEmail(email)).thenReturn(null);
+        Customer result = customerService.getCustomerByEmail(email);
+        assertNull(result);
+       // assertNull(customerService.getCustomerByEmail(""));
     }
 
     @Test
     void testGetCustomerByWrongId(){
-        assertNull(customerService.getCustomerById(-1));
+        int id = -1;
+        Customer customer = new Customer();
+        when(customerDAO.getCustomerById(id)).thenReturn(customer);
+        Customer result= customerService.getCustomerById(id);
+        assertNull(result);
+       // assertNull(customerService.getCustomerById(-1));
     }
     @Test
     void testGetCustomerByCorrectId(){
-        assertNotNull(customerService.getCustomerById(1));
-
+        int id = 1;
+        Customer customer = new Customer();
+        customer.setCustomer_fname("Sania");
+        customer.setCustomer_id(1);
+        customer.setCustomer_lname("kumar");
+        customer.setEmail("test@gmail.com");
+        customer.setPassword("password");
+        when(customerDAO.getCustomerById(id)).thenReturn(customer);
+        Customer result= customerService.getCustomerById(id);
+//        assertNotNull(customerService.getCustomerById(1));
+        assertTrue(result!=null);
     }
+
     @Test
     void testupdateAvaialableCredits(){
-        assertTrue(customerService.buyCredits(9,22));
+        int customer_id = 9;
+        Customer customer = new Customer();
+        customer.setCustomer_fname("Sania");
+        customer.setCustomer_id(1);
+        customer.setCustomer_lname("kumar");
+        customer.setEmail("test@gmail.com");
+        customer.setPassword("password");
+        when(customerDAO.getCustomerById(customer_id)).thenReturn(customer);
+        when(customerDAO.updateAvaialableCredits(customer_id,100)).thenReturn(true);
+        boolean result = customerService.buyCredits(customer_id,100);
+        assertTrue(result);
+        // assertTrue(customerService.buyCredits(9,22));
     }
 }
