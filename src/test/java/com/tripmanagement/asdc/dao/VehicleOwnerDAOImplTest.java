8075b86e5dbc8858e55a698df173862ee5aac294
Dharmay Dineshchandra Sureja <dh276903@dal.ca>
2022-04-02 14:56:13 -0300

    Add test case for VehicleOwnerDao

---
+++ src/test/java/com/tripmanagement/asdc/dao/VehicleOwnerDAOImplTest.java
@@ -0,0 +1,84 @@
+package com.tripmanagement.asdc.dao;
+
+import com.tripmanagement.asdc.model.VehicleOwner;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+class VehicleOwnerDAOImplTest {
+    @Autowired
+    VehicleOwnerDAO vehicleOwnerDAO;
+    @Test
+    void testsaveVehicleOwner() {
+//        "+null+",'"+vehicleOwner.getVehicleowner_fname()+"','"+vehicleOwner.getVehicleowner_lname()+"','"+vehicleOwner.getPhone()+"','"+vehicleOwner.getAddress()+"','"+vehicleOwner.getEmail()+"',"+vehicleOwner.getVehicleOwner_id()+",'"+vehicleOwner.getPassword()+"');";
+        VehicleOwner vehicleOwner = new VehicleOwner();
+        vehicleOwner.setVehicleowner_fname("test");
+        vehicleOwner.setVehicleowner_lname("case");
+        vehicleOwner.setEmail("test@email.test");
+        vehicleOwner.setPassword("123456");
+        vehicleOwner.setVehicleOwner_id(1111111);
+       // vehicleOwner.setAddress("testcase street");
+        vehicleOwner.setPhone("1234567890");
+        assertTrue(vehicleOwnerDAO.saveVehicleOwner(vehicleOwner));
+    }
+    @Test
+    void testsaveVehicleOwnerIllegalname() {
+//        "+null+",'"+vehicleOwner.getVehicleowner_fname()+"','"+vehicleOwner.getVehicleowner_lname()+"','"+vehicleOwner.getPhone()+"','"+vehicleOwner.getAddress()+"','"+vehicleOwner.getEmail()+"',"+vehicleOwner.getVehicleOwner_id()+",'"+vehicleOwner.getPassword()+"');";
+        VehicleOwner vehicleOwner = new VehicleOwner();
+        vehicleOwner.setVehicleowner_fname("test\'");
+        vehicleOwner.setVehicleowner_lname("case");
+        vehicleOwner.setEmail("test@email.test");
+        vehicleOwner.setPassword("123456");
+        vehicleOwner.setVehicleOwner_id(1111111);
+       // vehicleOwner.setAddress("testcase street");
+        vehicleOwner.setPhone("1234567890");
+        assertFalse(vehicleOwnerDAO.saveVehicleOwner(vehicleOwner));
+    }
+
+    @Test
+    void testsaveVehicleOwner_null(){
+        VehicleOwner vehicleOwner = new VehicleOwner();
+        assertFalse(vehicleOwnerDAO.saveVehicleOwner(vehicleOwner));
+
+    }
+
+    @Test
+    void testGetCorrectVehicleOwnerByEmail() {
+        assertNotNull(vehicleOwnerDAO.getVehicleOwnerByEmail("test@email.test"));
+    }
+    @Test
+    void testGetWrongVehicleOwnerbyEmail() {
+        assertNull(vehicleOwnerDAO.getVehicleOwnerByEmail("test1@test1.test1"));
+    }
+
+    @Test
+    void testGetNullVehicleOwnerbyEmail() {
+        assertNull(vehicleOwnerDAO.getVehicleOwnerByEmail(null));
+    }
+
+    @Test
+    void testGetEmptyVehicleOwnerbyEmail() {
+        assertNull(vehicleOwnerDAO.getVehicleOwnerByEmail(null));
+    }
+
+    @Test
+    void testgetVehicleOwnerById_true() {
+        assertTrue(vehicleOwnerDAO.getVehicleOwnerById(13)!=null);
+    }
+
+    @Test
+    void testgetVehicleOwnerById_false() {
+        assertNull(vehicleOwnerDAO.getVehicleOwnerById(00));
+    }
+
+    @Test
+    void testupdateAvaialableCredits_correct(){
+        assertTrue(vehicleOwnerDAO.updateAvaialableCredits(42,552));
+    }
+
+}
