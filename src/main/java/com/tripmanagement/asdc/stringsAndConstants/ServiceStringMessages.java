92d4aeb9ba8c86a7ac5c7109814fb57d4be0eb3c
Prakrut <pr298346@dal.ca>
2022-04-03 15:11:58 -0300

    Update messages in ServiceStringMessages

--- src/main/java/com/tripmanagement/asdc/stringsAndConstants/ServiceStringMessages.java
+++ src/main/java/com/tripmanagement/asdc/stringsAndConstants/ServiceStringMessages.java
@@ -6,7 +6,7 @@ public final class ServiceStringMessages {
     public static final String INCORRECT_AUTH="Incorrect email or password";
     public static final String NO_USER_FOUND="User does not exist";
     public static final String SUCCESS="Success";
-    public static final String FAILURE="Success";
+    public static final String FAILURE="Failure";
     public static final String USER_REGISTERED_SUCCESSFULLY=", You are registered successfully";
     public static final String AUTH_SUCCESSFUL="Authentication Successful";
     public static final String USER_ALREADY_EXIST = "User already exists";
