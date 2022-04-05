2750528826232399fec9ba96ae5b28e6f9371696
Prakrut <pr298346@dal.ca>
2022-04-05 19:13:45 -0300

    Add class ServiceConstants and Move constants from Constants.java

--- src/main/java/com/tripmanagement/asdc/stringsAndConstants/ServiceConstants.java
+++ src/main/java/com/tripmanagement/asdc/stringsAndConstants/ServiceConstants.java
@@ -1,4 +1,11 @@
 package com.tripmanagement.asdc.stringsAndConstants;
 
 public class ServiceConstants {
+
+    public static final String USER_TYPE_CUSTOMER ="CUSTOMER";
+    public static final String USER_TYPE_VEHICLE_OWNER ="VEHICLE_OWNER";
+    public static final String GOOD="GOOD";
+    public static final String BAD="BAD";
+    public static final String AVERAGE="AVERAGE";
+    public static final int INITIAL_CREDITS=500;
 }
