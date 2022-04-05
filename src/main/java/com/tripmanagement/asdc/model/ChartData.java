2f9892ef7fb4fc98bc858a576404ec4c4596826c
Vatsal Yadav <vt769414@dal.ca>
2022-04-04 22:02:00 -0300

    Add Fuel consumed and Kms travelled charts

---
+++ src/main/java/com/tripmanagement/asdc/model/ChartData.java
@@ -0,0 +1,40 @@
+package com.tripmanagement.asdc.model;
+
+public class ChartData {
+    public ChartData() {
+    }
+
+    String[] label;
+    String[] backgroundColor;
+    float[] data;
+
+    public ChartData(String[] label, String[] backgroundColor, float[] data) {
+        this.label = label;
+        this.backgroundColor = backgroundColor;
+        this.data = data;
+    }
+
+    public String[] getLabel() {
+        return label;
+    }
+
+    public void setLabel(String[] label) {
+        this.label = label;
+    }
+
+    public String[] getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String[] backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public float[] getData() {
+        return data;
+    }
+
+    public void setData(float[] data) {
+        this.data = data;
+    }
+}
